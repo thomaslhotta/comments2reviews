fb8ad07efdab0d34991d2bd574080f98639d1082
Tom <th.lhotta@gmail.com>
2016-07-29 14:23:31 +0200

    Fixed issue with HHVM

--- classes/class-mycred-review.php
+++ classes/class-mycred-review.php
@@ -41,8 +41,49 @@ class MyCRED_Review extends myCRED_Hook_Comments {
 			),
 		);
 
-		$grand_parent = new ReflectionClass( get_parent_class( get_parent_class( $this ) ) );
-		$grand_parent->getConstructor()->invoke( $this, $args, $hook_prefs, $type );
+		if ( ! empty( $args ) ) {
+			foreach ( $args as $key => $value ) {
+				$this->$key = $value;
+			}
+		}
+
+		// Grab myCRED Settings
+		$this->core        = mycred( $type );
+		$this->point_types = mycred_get_types();
+
+		if ( $type != '' ) {
+			$this->core->cred_id = sanitize_text_field( $type );
+			$this->mycred_type   = $this->core->cred_id;
+		}
+
+		if ( $this->mycred_type != 'mycred_default' ) {
+			$this->is_main_type = false;
+		}
+
+		// Grab settings
+		if ( $hook_prefs !== null ) {
+
+			// Assign prefs if set
+			if ( isset( $hook_prefs[ $this->id ] ) ) {
+				$this->prefs = $hook_prefs[ $this->id ];
+			}
+
+			// Defaults must be set
+			if ( ! isset( $this->defaults ) ) {
+				$this->defaults = array();
+			}
+
+		}
+
+		// Apply default settings if needed
+		if ( ! empty( $this->defaults ) ) {
+			$this->prefs = mycred_apply_defaults( $this->defaults, $this->prefs );
+		}
+
+		// HHVM has issues with this
+
+		//$grand_parent = new ReflectionClass( get_parent_class( get_parent_class( $this ) ) );
+		//$grand_parent->getConstructor()->invoke( $this, $args, $hook_prefs, $type );
 	}
 
 	/**
